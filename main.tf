896d14ed195a47e69a9c99fdf4dd3098112f6b45
Maximilian Schäfers <maximilian.schaefers@gmx.de>
2025-04-25 22:52:48 +0200

    change: install metallb via terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "> 0.8.0"
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
     helm = {
       source = "hashicorp/helm"
       version = "3.0.0-pre2"
@@ -16,14 +20,35 @@ resource "null_resource" "create_kind_cluster" {
   provisioner "local-exec" {
     when = destroy
     command = "kind get clusters | xargs -t -n1 kind delete cluster --name"
- }
+  }
 }
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "helm_release" "metallb" {
+  name       = "metallb"
+  namespace  = "metallb-system"
+  chart = "./metallb"
+  create_namespace = "true"
+}
+resource "kubernetes_manifest" "metallb_pool" {
+  manifest = yamldecode(file("${path.module}/metallb/pool.yaml"))
+  depends_on = [helm_release.metallb]
+}
+resource "kubernetes_manifest" "metallb_advertisement" {
+  manifest = yamldecode(file("${path.module}/metallb/advertisement.yaml"))
+  depends_on = [helm_release.metallb]
+}
+
 
 provider "helm" {
   kubernetes = {
     config_path = "~/.kube/config"
   }
 }
+
 resource "helm_release" "istiod" {
   name       = "istio"
   repository = "https://istio-release.storage.googleapis.com/charts"
